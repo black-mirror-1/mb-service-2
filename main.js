ee96254326a591f2649e1505cae215564407b7ec
Palavalasa <palavas@f8ffc201c437.ant.amazon.com>
2020-05-27 17:03:29 -0700

    Added V5 to message

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const server = http.createServer((_, res) => {
       <title>mb-service-2</title> \
   </head> \
   <body> \
-      <h1>Hello, you have reached mb-service-2! V4</h1> \
+      <h1>Hello, you have reached mb-service-2! V5</h1> \
   </body> \
    </html>');
 });
